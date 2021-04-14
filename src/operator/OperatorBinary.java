d4b8d7c97c9a5d17ff30a591e9e96f0a14bfd981
abdulfatah93 <fatah4318@gmail.com>
2021-04-14 20:24:05 +0700

    Operator Binary aritmatika

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class OperatorBinary {
+    String nama = "Abdul Fatah";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.90;
+
+    public static void main(String[]args){
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+}
