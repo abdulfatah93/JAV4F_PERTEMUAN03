3d6c1073979651149c6b25c4cb764eb96dbb3cd5
abdulfatah93 <fatah4318@gmail.com>
2021-04-14 22:46:41 +0700

    condition true sehingga statement1 jalan

---
+++ src/percabangan/ifElse.java
@@ -0,0 +1,15 @@
+package percabangan;
+
+public class ifElse {
+    public static void main(String[]args){
+        double totalBelanja =53000;
+        double uangDidompet =25000;
+
+        if(uangDidompet<totalBelanja){
+            System.out.println("uang kada cukup,kurangi belanjaan anda");
+        }else {
+            System.out.println("alhamdulillah uang cukup haja, selamat menikmati");
+        }
+        }
+    }
+
