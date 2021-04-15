fa4629c6c9e5459213ba559ef9d5d89f630dab85
abdulfatah93 <fatah4318@gmail.com>
2021-04-15 13:02:02 +0700

    percabangan  switch Mhs ngada-ngada

---
+++ src/percabangan/switch2.java
@@ -0,0 +1,31 @@
+package percabangan;
+
+public class switch2 {
+    public static void main(String[]args){
+        char nilai='E';
+        switch (nilai){
+            case 'A':
+                System.out.println("Mhs   : \"Terimakasih pak\"");
+                System.out.println("Dosen : \"Selamat ya !!\"");
+                break;
+            case 'B':
+                System.out.println("Mhs   : \"Kenapa ulun kda dapat A, pak\"");
+                System.out.println("Dosen : \"!#@$%&\"");
+                break;
+            case 'C':
+                System.out.println("Mhs   : \"Ulun turun pul pak ae,tugas pul jua\"");
+                System.out.println("Dosen : \"Tapi bisalah menjawab ujian?\"");
+                System.out.println("Mhs   : \"hihihihi....\"");
+                break;
+            default:
+                System.out.println("Mhs   : \"Ulun tutun pul pak ae,tugas pul jua\"");
+                System.out.println("Dosen : \"Bujur jua kah?\"");
+                System.out.println("Dosen : \"*memeriksa berkas\"");
+                System.out.println("Dosen : \"menceleng\"");
+                System.out.println("Mhs   : \"Kabur...\"");
+                break;
+
+
+        }
+    }
+}
