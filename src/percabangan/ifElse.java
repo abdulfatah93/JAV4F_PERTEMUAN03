31f9a1e8c4054f1056a5e0a69e814beef3ba5517
abdulfatah93 <fatah4318@gmail.com>
2021-04-15 12:29:36 +0700

    condition1 false dan condition2 true sehingga statement2 jalan

--- src/percabangan/ifElse.java
+++ src/percabangan/ifElse.java
@@ -5,10 +5,13 @@ public class ifElse {
         double totalBelanja =53000;
         double uangDidompet =25000;
 
-        if(uangDidompet<totalBelanja){
+        if(uangDidompet>totalBelanja){
             System.out.println("uang kada cukup,kurangi belanjaan anda");
+        }else if (uangDidompet<totalBelanja){
+            double angsul = uangDidompet-totalBelanja;
+            System.out.println("alhamdulillah uang cukup haja,ansul : "+angsul);
         }else {
-            System.out.println("alhamdulillah uang cukup haja, selamat menikmati");
+            System.out.println("uang pas ..yuhuuu..");
         }
         }
     }
